97dd75aaa5e66a9e1276024a29f89e9e8bd490c7
Herzeg.uz <110915492+Herzeg137@users.noreply.github.com>
2023-03-11 10:49:24 +0500

    Create heap_sort2.py

---
+++ heap_sort2.py
@@ -0,0 +1,28 @@
+class Solution:
+    def heap_sort(self, lst: List[int]) -> None:
+        def max_heapify(heap_size, index):
+            left, right = 2 * index + 1, 2 * index + 2
+            largest = index
+            if left < heap_size and lst[left] > lst[largest]:
+                largest = left
+            if right < heap_size and lst[right] > lst[largest]:
+                largest = right
+            if largest != index:
+                lst[index], lst[largest] = lst[largest], lst[index]
+                max_heapify(heap_size, largest)
+
+        # heapify original lst
+        for i in range(len(lst) // 2 - 1, -1, -1):
+            max_heapify(len(lst), i)
+
+        # use heap to sort elements
+        for i in range(len(lst) - 1, 0, -1):
+            # swap last element with first element
+            lst[i], lst[0] = lst[0], lst[i]
+            # note that we reduce the heap size by 1 every iteration
+            max_heapify(i, 0)
+            
+            
+#code is from leetcode explore
+#sort: heap sort
+#t: O(n logn), s: O(n)
